9e293fb3fc2d7726ce2c60f78d26b917aaa78621
Remy-Makota <114928586+Remy-Makota@users.noreply.github.com>
2024-08-13 07:06:57 +0300

    Update ball.py

--- ball.py
+++ ball.py
@@ -19,19 +19,23 @@ class Ball(Turtle):
         self.x_move = 10
         self.y_move = 10
         self.move_speed = 0.1
-        
+
+    # Function to move the ball
     def move_ball(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
-        
+
+    # Function to move the ball in the y direction
     def bounce_y(self):
         self.y_move *= -1
-        
+
+    # Function to move the ball in the x direction
     def bounce_x(self):
         self.x_move *= -1
         self.move_speed *= 0.9
-    
+
+    # Function to reset after missing the paddle
     def reset_position(self):
         self.home()
         self.move_speed = 0.1
