f8613e079eb7479628cb2c4ee6c573a53e16d4db
Remy-Makota <114928586+Remy-Makota@users.noreply.github.com>
2024-08-13 07:12:35 +0300

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -17,18 +17,21 @@ class ScoreBoard(Turtle):
         self.l_score = 0
         self.r_score = 0
         self.update_scoreboard()
-        
+
+    # Function to update the scoreboard after every play
     def update_scoreboard(self):
         self.clear()
         self.goto(-100, 200)
         self.write(self.l_score, align= 'center', font= ('Courier', 80, 'normal'))
         self.goto(100, 200)
         self.write(self.r_score, align= 'center', font= ('Courier', 80, 'normal'))
-        
+
+    # Function to update the left paddle score
     def l_point(self):
         self.l_score += 1
         self.update_scoreboard()
-        
+
+    # Function to update the right paddle score
     def r_point(self):
         self.r_score += 1
         self.update_scoreboard()
